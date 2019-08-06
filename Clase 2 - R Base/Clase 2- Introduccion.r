51dc02e5780e7fe6be7b1e8686ccd98b0b351978
PabloT <pablotisco@gmail.com>
2019-08-06 12:15:45 -0300

    edito script de Clase 2

--- Clase 2 - R Base/Clase 2- Introduccion.r
+++ Clase 2 - R Base/Clase 2- Introduccion.r
@@ -1,21 +1,44 @@
 rm(list=ls())
 
+# Defino el objeto "A" con el contenido "1".
 A <- 1
 
+# Al definir un elemento, el mismo queda guardado en el ambiente del programa, y podrá ser utilizado posteriormente para observar su contenido o para realizar una operación con el mismo
 A 
 A+6
+
+# Al correr una linea con el nombre del objeto, la consola del programa nos muestra 
+# su contenido. Entre Corchetes Observamos el número de orden del elemento en cuestión
+# El operador = es equivalente a <-, pero en la práctica no se utiliza 
+# para la definición de objetos.   
 B = 2
 B
+
+# <- es un operador Unidireccional, es decir que:     
+A <- B # implica que A va tomar como valor el contenido del objeto B, y no al revés.
+
 A <- B
 A   #Ahora A toma el valor de B, y B continua conservando el mismo valor
 B
-## R base
 
+## R base
+# Con R base nos referimos a los comandos básicos que vienen incorporados en el R,
+# sin necesidad de cargar librerías. 
+
+# Operadores lógicos: 
+
+# $>$
+# $>=$
+# $<$
+# $<=$
+# $==$
+# $!=$
+  
 #Redefinimos los valores A y B
 A <-  10
 B  <-  20
-#Realizamos comparaciones lógicas
 
+#Realizamos comparaciones lógicas
 A >  B
 A >= B
 A <  B
@@ -25,6 +48,10 @@ A != B
 
 C <- A != B
 C
+
+# Como muestra el último ejemplo, el resultado de una operación lógica puede 
+# almacenarse como el valor de un objeto.
+
 ## Operadores aritméticos:
 
 #suma
@@ -41,6 +68,20 @@ D <- 6*2.5
 D
 
 ## Funciones:
+# Las funciones son series de procedimientos estandarizados, que toman como imput 
+# determinados argumentos a fijar por el usuario, y devuelven un resultado acorde 
+# a la aplicación de dichos procedimientos. Su lógica de funcionamiento es:   
+#  funcion(argumento1 = arg1, argumento2 = arg2)      
+
+# A lo largo del curso iremos viendo numerosas funciones, según lo requieran los 
+# distintos ejercicios. Sin embargo, veamos ahora algunos ejemplos para comprender 
+# su funcionamiento:    
+  
+# paste() : concatena una serie de caracteres, indicando por última instancia como separar a cada uno de ellos        
+# paste0(): concatena una serie de caracteres sin separar
+# sum(): suma de todos los elementos de un vector   
+# mean() promedio aritmético de todos los elementos de un vector   
+
 paste("Pega","estas",4,"palabras", sep = " ")
 
 #Puedo concatenar caracteres almacenados en objetos
@@ -55,20 +96,30 @@ sum(1:5)
 mean(1:5,na.rm = TRUE)
 
 
+## Objetos:    
+Valores
+- Vectores
+- Data Frames
+- Listas
+
+### Valores
+# Los valores y vectores pueden ser a su vez de distintas clases:
+  
+# Numeric     
 A <-  1
 class(A)
 
-
+# Character
 A <-  paste('Soy', 'una', 'concatenación', 'de', 'caracteres', sep = " ")
 A
 class(A)
 
+# Factor
 A <- factor("Soy un factor, con niveles fijos")
 class(A)
 
-
 ### Vectores
-
+# Para crear un vector utilizamos el comando c(), de combinar.
 C <- c(1, 3, 4)
 C
 
@@ -78,23 +129,22 @@ C
 D <- C + 1:3 #esto es equivalente a hacer 3+1, 5+2, 6+9 
 D
 
-
-
+#crear un vector que contenga las palabras: "Carlos","Federico","Pedro"
 E <- c("Carlos","Federico","Pedro")
 E
-
 
 E[2]
 
 elemento2 <-  E[2]
 elemento2
+
 rm(elemento2)
 elemento2
+
 E[2] <- "Pablo"
 E
 
 ### Data Frames
-
 INDICE  <- c(100,   100,   100,
              101.8, 101.2, 100.73,
              102.9, 102.4, 103.2)
@@ -116,7 +166,6 @@ Datos
 Datos$FECHA
 Datos[3,2]
 Datos$FECHA[3]
-
 Datos$FECHA[3,2]
 
 Datos[Datos$FECHA=="Dic-16",]
@@ -136,12 +185,16 @@ superlista
 
 superlista$DF$FECHA[2]
 
+# Instalación de paquetes complementarios al R Base          
+install.packages("nombre_del_paquete") 
+library(nombre_del_paquete)
 
 # Lectura y escritura de archivos
-
 ## .csv  y  .txt
-individual_t117 <- read.table(file = 'Fuentes/usu_individual_t117.txt',sep=";", dec=",", header = TRUE, fill = TRUE)
+dataframe <- read.delim(file, header = TRUE, sep = "\t", quote = "\"", dec = ".", fill = TRUE, comment.char = "", ...) 
 
+individual_t117 <- read.table(file = '../Fuentes/usu_individual_t117.txt',sep=";", dec=",", header = TRUE, fill = TRUE)
+individual_t117
 
 #View(individual_t117)
 names(individual_t117)
@@ -159,8 +212,8 @@ tabla_de_R <- data.frame(x,y)
 
 # escribimos el archivo
 write.xlsx( x = tabla_de_R, file = "archivo.xlsx",row.names = FALSE)
-#Donde lo guardó? Hay un directorio por default en caso de que no hayamos definido alguno.
-getwd()
+
+#getwd()
 
 #Si queremos exportar multiples dataframes a un Excel, debemos armar previamente una lista de ellos. Cada dataframe, se guardará en una pestaña de excel, cuyo nombre correspondera al que definamos para cada Dataframe a la hora de crear la lista.
 Lista_a_exportar <- list("Indices Salarios" = Datos,
@@ -173,4 +226,7 @@ Indices_Salario <- read.xlsx(xlsxFile = "archivo_2_hojas.xlsx",sheet = "Indices 
 #alternativamente podemos especificar el número de orden de la hoja que deseamos levantar
 Indices_Salario <- read.xlsx(xlsxFile = "archivo_2_hojas.xlsx",sheet = 1)
 Indices_Salario
+
+# Paquetes a utilizar a lo largo del curso:
+install.packages(c("tidyverse","openxlsx",'ggplot2','ggthemes', 'ggrepel','ggalt','kableExtra'))
 
