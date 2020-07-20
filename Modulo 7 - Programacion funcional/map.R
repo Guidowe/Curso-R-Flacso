a66d5c32a68d6f55ea6112a6f45873eea38b769b
Guidowe <guidowe45@gmail.com>
2020-07-20 14:27:51 -0300

    reorganizo repo

---
+++ Modulo 7 - Programacion funcional/map.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+letras.mayusc <- LETTERS[1:20]
+letras.minusc <- letters[1:20]
+numeros <- 1:20
+
+
+map(letras.mayusc,tolower)
+
+map2(letras,numeros,paste)
+
+map(letras,paste,numeros)
+
+OK<- data.frame(letras.mayusc,letras.minusc,numeros)
