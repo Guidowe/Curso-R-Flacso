c88a018ec00b8c83a6c521eb908cd91c17c4d198
Guidowe <guidowe45@gmail.com>
2021-02-02 16:03:26 -0300

    ggplot

---
+++ Modulo 6 - Strings/practica en clase.R
@@ -0,0 +1,5 @@
+##Ejercicio 3: Crear objetos que conserven únicamente:
+
+# Los registros cuyos tweets mencionen a Chile
+# Los registros cuyos tweets provengan de un dispositivo iPhone o iPad
+readRDS("")
